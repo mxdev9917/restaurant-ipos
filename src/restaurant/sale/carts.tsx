39197945c6d60180f6ef03992f5fc1e4034592e2
mxdev9917 <140176546+mxdev9917@users.noreply.github.com>
2024-09-12 21:40:10 +0700

    update all 12 09 2024

--- src/restaurant/sale/carts.tsx
+++ src/restaurant/sale/carts.tsx
@@ -89,9 +89,11 @@ function Carts() {
                                         $2999
                                     </td>
                                     <td class="px-3 py-4">
+                                       <button className="hover:bg-slate-100 p-1.5 rounded-full">
                                         <svg className="w-6 h-6 text-red-600 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                         </svg>
+                                        </button>
                                     </td>
                                 </tr>
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
@@ -106,9 +108,11 @@ function Carts() {
                                         $1999
                                     </td>
                                     <td class="px-3 py-4">
+                                        <button className="hover:bg-slate-100 p-1.5 rounded-full">
                                         <svg className="w-6 h-6 text-red-600 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                         </svg>
+                                        </button>
                                     </td>
                                 </tr>
                                 <tr class="bg-white dark:bg-gray-800">
@@ -123,9 +127,13 @@ function Carts() {
                                         $99
                                     </td>
                                     <td class="px-3 py-4">
+                                        <button>
+                                        <button className="hover:bg-slate-100 p-1.5 rounded-full">
                                         <svg className="w-6 h-6 text-red-600 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                         </svg>
+                                        </button>
+                                        </button>
                                     </td>
                                 </tr>
                             </tbody>
