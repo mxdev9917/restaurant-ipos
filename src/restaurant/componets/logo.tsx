328ef6bd0fc5570eea658081ba519b69a8ea647b
mxdev9917 <140176546+mxdev9917@users.noreply.github.com>
2024-09-24 21:39:54 +0700

    update

--- src/restaurant/componets/logo.tsx
+++ src/restaurant/componets/logo.tsx
@@ -1,13 +1,12 @@
-
-
+import iposLogo from "../../assets/images/ipos.png";
 function Logo() {
-    return <div>
-        <a href="/" className="flex ms-2 gap-1  md:me-24">
-            <img src="src/assets/images/ipos.png" className="h-11 " alt="FlowBite Logo" />
-            <span className="self-center text-xl pt-2 font-extrabold text-orange-500 sm:text-4xl whitespace-nowrap ">IPOS.LA</span>
-        </a>
-    </div>
+    return (
+        <div>
+            <a href="/" className="flex ms-2 gap-1 md:me-24">
+                <img src={iposLogo} className="h-11" alt="FlowBite Logo" />
+                <span className="self-center text-xl pt-2 font-extrabold text-orange-500 sm:text-4xl whitespace-nowrap ">
+                    IPOS.LA        </span>
+            </a> 
+        </div>);
 }
-
-
-export default Logo
+export default Logo;
