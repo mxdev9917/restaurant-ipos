e36489051fe5fc3552d95eabaf780778ebfbb4c2
mxdev9917 <140176546+mxdev9917@users.noreply.github.com>
2024-10-16 12:31:32 +0700

    edit bug react-router-hash-link

--- src/website/components/navbar.tsx
+++ src/website/components/navbar.tsx
@@ -1,49 +1,39 @@
-
-
-import Language from "./language"
-import Logo from "../../restaurant/components/logo"
+import Language from "./language";
+import Logo from "../../restaurant/components/logo";
 import { Button, Navbar } from "flowbite-react";
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";  // Correct import
 
 function Navbars() {
     return (
         <div className="w-full flex justify-center">
             <Navbar fluid rounded className="bg-[#3a393a] w-full xl:w-2/3 lx:w-3/3 xl:rounded-full rounded-none px-0 xl:px-8 ">
-                <Navbar.Brand >
+                <Navbar.Brand>
                     <Logo />
                 </Navbar.Brand>
 
                 <div className="flex items-center gap-2 md:order-2">
                     <Language />
-                    <Button className="focus:outline-none focus:ring-0 hover:text-orange-500 hover:border-orange-500">Sing in</Button>
+                    <Button className="focus:outline-none focus:ring-0 hover:text-orange-500 hover:border-orange-500">Sign in</Button>
                     <Navbar.Toggle className="flex bg-transparent hover:bg-transparent border-none text-sm focus:ring-0" />
-
                 </div>
 
                 <Navbar.Collapse className="hover:bg-transparent">
                     <Navbar.Link to="/home" active className="">
-                       <p className="pl-1 hover:text-orange-500"> Home</p>
+                        <p className="pl-1 hover:text-orange-500"> Home</p>
                     </Navbar.Link>
                     <Navbar.Link href="#" className="border-none text-white hover:bg-transparent">
-                       
-                        <p className="pl-1 hover:text-orange-500 ">Pricing</p>
+                        <p className="pl-1 hover:text-orange-500">Pricing</p>
                     </Navbar.Link>
                     <HashLink to="/#download" className="border-none text-white hover:bg-transparent ">
-                        
                         <p className="pl-1 hover:text-orange-500">Download App</p>
                     </HashLink>
                     <Navbar.Link href="#" className="border-none text-white hover:bg-transparent">
-                     
-                         <p className="pl-1 hover:text-orange-500">Contact us</p>
-                    </Navbar.Link> 
+                        <p className="pl-1 hover:text-orange-500">Contact us</p>
+                    </Navbar.Link>
                 </Navbar.Collapse>
-                {/* <HashLink to="/#download">Link to Hash Fragment</HashLink> */}
-              
-
-
             </Navbar>
         </div>
     );
 }
 
-export default Navbars
+export default Navbars;
