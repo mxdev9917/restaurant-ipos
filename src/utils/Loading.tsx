fc7719dfe8204707f1d6a1c110f87868d588665c
mxdev9917 <140176546+mxdev9917@users.noreply.github.com>
2024-11-15 21:39:29 +0700

    update all

---
+++ src/utils/Loading.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface LoadingProps {
+    text: string;
+}
+
+const Loading: React.FC<LoadingProps> = ({ text }) => {
+    return (
+        <div className="h-full flex justify-center items-end space-x-0">
+            <span className="font-medium mr-1">{text}</span>
+            <span className="font-medium animate-pulse animation-delay-700">.</span>
+            <span className="font-medium animate-pulse animation-delay-800">.</span>
+            <span className="font-medium animate-pulse animation-delay-900">.</span>
+            <span className="font-medium animate-pulse animation-delay-1000">.</span>
+        </div>
+    );
+};
+
+
+
+export default Loading;
